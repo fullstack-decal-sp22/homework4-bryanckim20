5e8861ad99c08ca992cfe9efaf8b291a20bb2bbb
Bryan Kim <bryanckim@berkeley.edu>
2022-03-22 17:04:14 -0700

    sub1

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/Board.css';
 import Square from "./Square";
 
 function Board() {
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [playerX, nextPlayer] = useState(true);
+    var status = 'Next player: X';
 
-    const status = 'Next player: X';
+    function handleClick(i) {
+      const curr = board.slice();
+      if (calculateWinner(curr) || curr[i]) {
+        return;
+      }
+      curr[i] = playerX ? 'X' : 'O';
+      setBoard(curr);
+      nextPlayer(!playerX);
+    };
+
+    function calculateWinner(board) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+          return board[a];
+        }
+      }
+      return null;
+    }
+
+    const winner = calculateWinner(board);
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (playerX ? 'X' : 'O');
+    }
+
+    
 
     function renderSquare(i) {
-        return <Square />;
+        return (
+          <Square
+          value = {board[i]}
+          onClick = {() => handleClick(i)}/>
+        );
     }
 
     return (  
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/Game.css';
 import Board from "./Board";
 
 function Game() {
-    
+
     return (
-        <div className="game">
-            <div className="game-board">
-                <Board />
+        <div className="game" >
+        <div className="game-board" >
+            <Board />
+            </div> 
             </div>
-        </div>
-    )
+            )
 }
 
-export default Game;
+            export default Game;
